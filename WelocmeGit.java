ab341fc1c8a7cfcc12c011b2414da9221e14a0b6
unknown <mindtreefeb58@iiht.tech>
2022-04-27 14:16:30 +0530

    add file

---
+++ WelocmeGit.java
@@ -0,0 +1,11 @@
+package jenkins_git;
+
+public class WelocmeGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Welocme Connection jenkins to git");
+	}
+
+}
