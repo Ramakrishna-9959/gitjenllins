0c77bd3504b5aa4cb38a84f29d3a6c04d9b3a670
unknown <mindtreefeb58@iiht.tech>
2022-04-28 13:00:36 +0530

    add new file

---
+++ Test2.java
@@ -0,0 +1,11 @@
+package jenkins_git;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+       System.out.println("Ram Krishna");		
+	}
+
+}
